b8463d5594f9ae0ed0c490bcb4f3291d4107850b
osama9 <osama.747@gmail.com>
2015-01-17 15:41:53 +0300

    Fixed bug with user profile validation

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -28,12 +28,22 @@ class UserTest < ActiveSupport::TestCase
   end
 
   test "a user should have a profile name without spaces" do
-  	user = User.new
+  	user = User.new(first_name: 'osama', last_name: 'ansari', email: 'osama2@gmail.com')
+    user.password = user.password_confirmation = 'asdfasdf'
+    
   	user.profile_name = "My Profile with spaces"
 
   	assert !user.save
   	assert !user.errors[:profile_name].empty?
   	assert user.errors[:profile_name].include?("Must be formatted correctly.")
+  end
+
+  test "a user can have a correctly formatted profile name" do
+    user = User.new(first_name: 'osama', last_name: 'ansari', email: 'osama2@gmail.com')
+    user.password = user.password_confirmation = 'asdfasdf'
+
+    user.profile_name = 'osama_3'
+    assert user.valid?
   end
 
 end
